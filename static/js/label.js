efb3f62b0ba865373f9dd06aa87b798fce499f59
gsnlyd <48926197+gsnlyd@users.noreply.github.com>
2020-07-20 01:20:46 -0400

    Clean up duplicated labeling code

---
+++ static/js/label.js
@@ -0,0 +1,71 @@
+// Time Tracking
+
+let startTime = Date.now();
+
+function getTimeTaken() {
+    return Date.now() - startTime;
+}
+
+function resetTimeTaken() {
+    startTime = Date.now();
+}
+
+// Constants
+
+let labelControls = {};
+for (const controlEl of document.querySelectorAll('.label-control')) {
+    labelControls[controlEl.dataset.controlIndex] = controlEl;
+}
+
+// Label Functions
+
+async function setLabel(elementId, labelValue) {
+    const setLabelJson = {
+        'element_id': elementId,
+        'label_value': labelValue,
+        'ms': getTimeTaken()
+    };
+    const rawResponse = await fetch('/api/set-label-value', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(setLabelJson)
+    });
+
+    if (!rawResponse.ok) {
+        console.log('Label failed');
+        return;
+    }
+    console.log('Label succeeded');
+
+    for (const controlEl of Object.values(labelControls)) {
+        controlEl.classList.toggle('selected', controlEl.dataset.labelValue === labelValue)
+    }
+
+    resetTimeTaken();
+}
+
+// Event Listeners
+
+document.addEventListener('keydown', ev => {
+    // Ignore within text input
+    if (ev.target.nodeName === 'INPUT') {
+        return;
+    }
+
+    // Label
+    if (ev.code.startsWith('Digit')) {
+        const num = parseInt(ev.code.substring(5, 6)) - 1;
+        if (num in Object.keys(labelControls)) {
+            labelControls[num].click();
+        }
+    }
+});
+
+for (const controlEl of Object.values(labelControls)) {
+    controlEl.addEventListener('click', ev => {
+        setLabel(controlEl.dataset.elementId, controlEl.dataset.labelValue);
+    })
+}
